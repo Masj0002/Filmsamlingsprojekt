2e58b97bfd7a32f5dd7e022d7c7da035c2613726
Masj0002 <Masj0002@stud.kea.dk>
2024-04-03 10:07:12 +0200

    Commit6

---
+++ src/ArrayListLæring.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+/*
+public class ArrayListLæring {
+    public static void main(String[] args) {
+
+
+    /*public static void main(String[] args) {
+
+        //Den indeholder kun Objekter
+
+        ArrayList<String> frugter = new ArrayList();
+        //når der er <String> på kan man lave string metoder.
+        frugter.add("Æble");
+        frugter.add("Banan");
+        System.out.println(frugter);
+        int antalFrugter = frugter.get(0).length();
+        System.out.println(antalFrugter);
+
+         Movie addMovie = new Movie(String "title", String "director", int year, boolean isInColor, int lengthInMinutes, String "genre");
+        ArrayList<Movies> film = new ArrayList<Movies>();
+         film.add
+
+
+        // i stedet for boolean true or false  kan man få den til at sige yes or no
+        Scanner scanner = new Scanner(System.in);
+        boolean isInColor = false; //nå er den sort/hvid
+        System.out.println("Er filmen i farve ja eller nej?");
+        String erIFarve = scanner.next();
+        erIFarve = erIFarve.toLowerCase(); //Laver alle bogstaver til lowerCase
+        if (erIFarve.equals("ja")) {
+            isInColor = true;
+        }
+
+        //to string metode
+        @Override
+        public String toString {
+            result = "";
+            result += title + " " + genre;
+            if (erIFarve) {
+                result += "er i farve";
+            }
+            return result;
+        }
+    }
+}
+*/
